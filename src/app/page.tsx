20f1e2ad324429a048c153da4e55d9f4dca357f1
ramirolepori <leporiramiro@gmail.com>
2024-10-24 20:46:37 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic';
-
-
 "use client";
+
+
+export const dynamic = 'force-dynamic';
 
 import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
